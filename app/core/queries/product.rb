f04d7cbeae1503dbd52f19b15821f814a0f0b14e
mark-jabczynski <61704195+mark-jabczynski@users.noreply.github.com>
2023-12-05 13:46:50 +0100

    Add UpdateDiscount subaction (#3)

--- app/core/queries/product.rb
+++ app/core/queries/product.rb
@@ -7,6 +7,17 @@ module Queries
     module Scopes
       include ::Query::Scopes
       include Concerns::ByCode
+
+      def with_discount_category
+        select(arel_table[Arel.star], discount_rule[:category].as('discount_category'))
+          .joins(:discount_rule)
+      end
+
+      private
+
+      def discount_rule
+        DiscountRule.arel_table
+      end
     end
   end
 end
--- app/models/discount_rule.rb
+++ app/models/discount_rule.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DiscountRule < ApplicationRecord
+  # TODO: Move to constants/configs/ymls
   enum category: {
     buy_one_get_one_free: 'BuyOneGetOneFree',
     buy_three_or_more_strawberries: 'BuyThreeOrMoreStrawberries',
